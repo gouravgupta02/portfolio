3d99dbca035db5b51f746e31d2eba37fea1b5e5d
Gourav Gupta <gouravguptagg02@gmail.com>
2024-03-06 16:57:25 +0530

    Updated Portfolio, experience, skills, image and css

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const scrollBtn = document.querySelector('.scroll-to-top');
 
 const refreshButtonVisibility = () => {
-    if (document.documentElement.scrollTop <= 150) {
+    if (document.documentElement.scrollTop <= 700) {
         scrollBtn.style.display = "none";
     } else {
         scrollBtn.style.display = "block";

948da6ec13adb0f8c6fdf4d1280a91829b99d74f
Gourav Gupta <gouravguptagg02@gmail.com>
2024-07-06 09:29:58 +0530

    updated js code to be executed after DOM is loaded, making back to top button code more readable.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,22 @@
-const scrollBtn = document.querySelector('.scroll-to-top');
+document.addEventListener("DOMContentLoaded", function() {
 
-const refreshButtonVisibility = () => {
-    if (document.documentElement.scrollTop <= 700) {
-        scrollBtn.style.display = "none";
-    } else {
-        scrollBtn.style.display = "block";
-    }
-};
-refreshButtonVisibility();
+    /* Logic for Scroll to top Button Start */
+    const scrollBtn = document.querySelector('.scroll-to-top');
 
-scrollBtn.addEventListener('click', () => {
-    document.documentElement.scrollTop = 0;
-});
+    const backToTopBtnShow = () => {
+        if (document.documentElement.scrollTop <= 700) {
+            scrollBtn.style.display = "none";
+        } else {
+            scrollBtn.style.display = "block";
+        }
+    };
+    backToTopBtnShow();
 
-document.addEventListener('scroll', (e) => {
-    refreshButtonVisibility();
+    scrollBtn.addEventListener('click', () => {
+        document.documentElement.scrollTop = 0;
+    });
+    document.addEventListener('scroll', (e) => {
+        backToTopBtnShow();
+    });
+    /* Logic for Scroll to top Button End */
 });
